b3cb46dce68c6f4cc0daafbd01b9bc7a1cacd616
Jefferson Henrique Bispo da Silva <64616693+Jeffenrique@users.noreply.github.com>
2021-07-01 16:15:05 -0300

    Update views.py

--- api/views.py
+++ api/views.py
@@ -11,12 +11,12 @@ def welcome(request):
 @api_view(["GET"])
 def users(request):
     users = [
-        {'nameDisciplina': 'Programação Web I','nameAluno': 'Paulo César', 'nota1': 10, 'nota2': 9, 'media': ( 10 + 9) / 2, 'situacao': 'Aprovado'},
-        {'nameDisciplina': 'Teste de Software', 'nameAluno': 'Paulo José', 'nota1': 3, 'nota2': 6, 'media': ( 3 + 6) / 2, 'situacao': 'Reprovado'},
-        {'nameDisciplina': 'Programação Web II', 'nameAluno': 'Francievellyn Larissa', 'nota1': 9, 'nota2': 9, 'media': ( 9 + 9) / 2, 'situacao': 'Aprovado'},
-        {'nameDisciplina': 'Programação Web III', 'nameAluno': 'Priscila Araujo', 'nota1': 5, 'nota2': 6, 'media': ( 5 + 6) / 2, 'situacao': 'Reprovado'},
+        {'nameDisciplina': 'Programação Web I','nameAluno': 'Jefferson Henrique', 'nota1': 10, 'nota2': 9, 'media': ( 10 + 9) / 2, 'situacao': 'Aprovado'},
+        {'nameDisciplina': 'Teste de Software', 'nameAluno': 'Jefferson Henrique', 'nota1': 3, 'nota2': 6, 'media': ( 3 + 6) / 2, 'situacao': 'Reprovado'},
+        {'nameDisciplina': 'Programação Web II', 'nameAluno': 'Jefferson Henrique', 'nota1': 9, 'nota2': 9, 'media': ( 9 + 9) / 2, 'situacao': 'Aprovado'},
+        {'nameDisciplina': 'Programação Web III', 'nameAluno': 'Jefferson Henrique', 'nota1': 5, 'nota2': 6, 'media': ( 5 + 6) / 2, 'situacao': 'Reprovado'},
         {'nameDisciplina': 'Lógica de Programação', 'nameAluno': 'Jefferson Henrique', 'nota1': 10, 'nota2': 8, 'media': ( 10 + 8 ) / 2, 'situacao': 'Aprovado'},
-        {'nameDisciplina': 'Programação Web I', 'nameAluno': 'Lucas Silva', 'nota1': 7, 'nota2': 7, 'media': ( 7 + 7 ) / 2, 'situacao': 'Aprovado'}
+        {'nameDisciplina': 'Programação Java', 'nameAluno': 'Jefferson Henrique', 'nota1': 7, 'nota2': 7, 'media': ( 7 + 7 ) / 2, 'situacao': 'Aprovado'}
     ]
 
     return JsonResponse(users, safe=False)
